9dfef273e401ac750f723c244aa0306e7cd85290
Andrew Gottlieb <andrewgottlieb@Andrews-Air.home>
2017-12-06 15:33:10 -0500

    styled contact section and adjusted project image sizes

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
 $(() => {
     console.log('js connected');
 
+    const mediapics = ["./images/mediaddict.png", "./images/twinpeaks.png", "./images/addshow.png", "./images/leaderboard.png"],
+    	pelspics = ["./images/pels.png", "./images/map.png", "./images/events.png", "./images/order.png"],
+    	bandpics = ["./images/band.png", "./images/mainpage.png", "./images/profile.png", "./images/explore.png"],
+    	scrabblepics = ["./images/scrabble.png", "./images/Instructions.png", "./images/inplay.png", "./images/Letters.png"];
+
+    let count = 0;
+    setInterval(function() {
+    	count++;
+    	if (count===4) {
+    		count=0;
+    	}
+    	$("#pels").attr("src", pelspics[count]);
+    	$("#mediaddict").attr("src", mediapics[count]);
+    	$("#bandessentials").attr("src", bandpics[count]);
+    	$("#scrabble").attr("src", scrabblepics[count]);
+
+    }, 1000);
+
 })
